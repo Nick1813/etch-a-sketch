8dd7aad36552c8d5c3af771113f0124bfa145dd2
Nick1813 <142940269+Nick1813@users.noreply.github.com>
2023-10-31 00:18:45 -0700

    added file and linked files

--- sketch.js
+++ sketch.js
@@ -1,2 +1,9 @@
 var n=25;
-
+for(let i=0;i<=n;i++){
+    document.body.innerHTML+='<div class="row">';
+    
+    for(let j=0;i<=n;j++){
+        document.body.innerHTML+='div class="gridsquare">' + (i*5+j+1) +'</div>';
+    }
+    document.body.innerHTML+='</div>';
+}
